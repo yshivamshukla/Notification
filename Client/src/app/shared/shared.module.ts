d4f57f422b9ee5fa60019776611a41c53c4d2835
ishivamshukla <shuklacrazy@gmail.com>
2019-10-06 21:41:56 +0530

    First commit

---
+++ Client/src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent, NavbarComponent, FooterComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
